ce88b4cd6744ba1c01bce3393e629c96b859842a
thedemonsid <siddheshshrirame@gmail.com>
2024-04-25 04:32:52 +0530

    Adding POST method to blogs route to update the Blogs based on Id

--- src/app/api/blogs/[blogId]/route.js
+++ src/app/api/blogs/[blogId]/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/dbConfig/dbConfig";
 import Blog from "@/models/blogModel";
-
+// Getting the particular blog by Id
 export async function GET(request, context) {
   try {
     await dbConnect();
@@ -23,7 +23,7 @@ export async function GET(request, context) {
     if (!blog) {
       return NextResponse.json(
         {
-          message: "Blog not found",
+          message: "No Blog found with this Id",
           success: false,
         },
         { status: 404 }
@@ -43,6 +43,49 @@ export async function GET(request, context) {
     });
   }
 }
+// Updating the blogs by Id
+export async function POST(request, context) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const { params } = context;
+    const id = params.blogId;
+    let blog;
+    try {
+      blog = await Blog.findByIdAndUpdate(id, body);
+    } catch (error) {
+      return NextResponse.json(
+        {
+          message: "Blog not found and not able to update",
+          success: false,
+        },
+        { status: 400 }
+      );
+    }
+    if (!blog) {
+      return NextResponse.json(
+        {
+          message: "No blog found with this ID",
+          success: false,
+        },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      {
+        message: "Blog found and updated",
+        newBlog: blog,
+        success: true,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({
+      error,
+    });
+  }
+}
+// Deleting the Blogs by Id
 export async function DELETE(request, context) {
   try {
     await dbConnect();
@@ -64,7 +107,7 @@ export async function DELETE(request, context) {
     if (!blog) {
       return NextResponse.json(
         {
-          message: "Blog not found",
+          message: "No Blog with this Id exists",
           success: false,
         },
         { status: 404 }
