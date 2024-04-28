2300e0f73cdf1882801f951a399041d8729f676e
thedemonsid <siddheshshrirame@gmail.com>
2024-04-29 04:30:51 +0530

    Add Denied page component

---
+++ src/app/denied/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Denied = () => {
+  return (
+    <div className="text-red-600 font-techy text-center p-2 m-2 h-screen">Denied</div>
+  );
+};
+
+export default Denied;
